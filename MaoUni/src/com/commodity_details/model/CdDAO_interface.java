8c7394f0c5089e16a0b8d0d9eec1b565819f9bfe
EstherZhong <mountain60237@gmail.com>
2021-08-04 14:21:23 +0800

    Fix: 思瑤修改

--- MaoUni/src/com/commodity_details/model/CdDAO_interface.java
+++ MaoUni/src/com/commodity_details/model/CdDAO_interface.java
@@ -11,6 +11,7 @@ public interface CdDAO_interface {
     public CdVO findByPrimaryKey(Integer CdOId);
     
     public List<CdVO> getAll();
+    public List<CdVO> cdpush(Integer CdOId);//在訂單跳小視窗用的明細
     
 //商品訂單與明細同時新增↓↓
 	void insert2(CdVO CdVO, Connection con);
